53265c037e6ed8bbfcfda5ae8396a688a6e4ef0f
Andronnix <a.d.kokorev@yandex.ru>
2021-05-13 21:33:10 +0300

    Gimble controls added.

--- rc_car/main.py
+++ rc_car/main.py
@@ -1,18 +1,81 @@
+""" Docs at: https://hubmodule.readthedocs.io/en/latest/ """
 import time
 
 from rshell import pyboard
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, KeyCode, Listener
+
+
+KeyW = KeyCode.from_char('w')
+KeyA = KeyCode.from_char('a')
+KeyS = KeyCode.from_char('s')
+KeyD = KeyCode.from_char('d')
+
+
+def drive():
+    if not keys[Key.up] and not keys[Key.down]:
+        left = 0
+        right = 0
+    elif keys[Key.down]:
+        left = -1
+        right = -1
+    else:  # Up
+        left = 1
+        right = 1
+
+    if keys[Key.left]:
+        left = left * 0.5
+
+    if keys[Key.right]:
+        right = right * 0.5
+
+    left = int(left * 100)
+    right = int(right * 100)
+
+    return "hub.port.E.motor.pwm({});hub.port.F.motor.pwm({})".format(-left, right)
+
+
+def gimble_ctrl():
+    H = 60
+    V = 60
+    horizontal = 0
+    vertical = 0
+
+    if keys[KeyA]:
+        horizontal = -H
+    elif keys[KeyD]:
+        horizontal = H
+
+    if horizontal == 0:
+        if keys[KeyW]:
+            vertical = V
+        elif keys[KeyS]:
+            vertical = -V
+    else:
+        vertical = int(-horizontal / 3)
+
+    return "hub.port.A.motor.pwm({});hub.port.B.motor.pwm({});".format(vertical, horizontal)
 
 
 if __name__ == "__main__":
-    board = pyboard.Pyboard("COM5")
+    print("Connecting")
+    board = pyboard.Pyboard("COM3")
     board.enter_raw_repl()
 
     board.exec_raw("import hub")
 
     print("Connected")
 
-    keys = {Key.left: False, Key.right: False, Key.up: False, Key.down: False, Key.shift: False}
+    keys = {Key.left: False,
+            Key.right: False,
+            Key.up: False,
+            Key.down: False,
+            Key.shift: False,
+            Key.esc: False,
+            KeyW: False,
+            KeyA: False,
+            KeyS: False,
+            KeyD: False,
+    }
 
     def on_press(key):
         if key in keys:
@@ -22,46 +85,24 @@ if __name__ == "__main__":
         if key in keys:
             keys[key] = False
 
-    prev_left = 0
-    prev_right = 0
-    prev_command = 0
-
-    # Collect events until released
+    prev_command = time.monotonic_ns()
     with Listener(on_press=on_press, on_release=on_release) as listener:
-        print("Starting..")
-        while not keys[Key.shift]:
-            if not keys[Key.up] and not keys[Key.down]:
-                left = 0
-                right = 0
-            elif keys[Key.down]:
-                left = -1
-                right = -1
-            else:  # Up
-                left = 1
-                right = 1
+        print("Started listening")
+        while not keys[Key.esc]:
+            drive_command = drive()
+            gimble_command = gimble_ctrl()
 
-            if keys[Key.left]:
-                left = left * 0.5
+            delta = time.monotonic_ns() - prev_command
+            if delta > 500:
+                board.exec_raw(str.encode(drive_command + ";" + gimble_command))
 
-            if keys[Key.right]:
-                right = right * 0.5
+                prev_command = time.monotonic()
+            else:
+                print(delta)
 
-            left = int(left * 100)
-            right = int(right * 100)
-            delta = time.clock() - prev_command
+        listener.stop()
+        print("Stopped listening")
 
-            if delta > 0.05:
-                command = "hub.port.E.motor.pwm({});hub.port.F.motor.pwm({})"\
-                    .format(-left, right)
-                board.exec_raw(str.encode(command))
-
-                prev_left = left
-                prev_right = right
-
-                prev_command = time.clock()
-
-
-        listener.join()
-
-    print("exiting")
+    print("Exiting")
     board.exit_raw_repl()
+    print("Done")
