39ee891284c66df54b1dd2cda1f7f22082c0cfb5
Andronnix <a.d.kokorev@yandex.ru>
2021-05-16 14:35:08 +0300

    Added detection point 3d-viz

--- rc_car/main.py
+++ rc_car/main.py
@@ -1,14 +1,20 @@
 """ Docs at: https://hubmodule.readthedocs.io/en/latest/ """
 import time
+import math
 
 from rshell import pyboard
 from pynput.keyboard import Key, KeyCode, Listener
 
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 KeyW = KeyCode.from_char('w')
 KeyA = KeyCode.from_char('a')
 KeyS = KeyCode.from_char('s')
 KeyD = KeyCode.from_char('d')
+KeyP = KeyCode.from_char('p')
+KeyO = KeyCode.from_char('o')
 
 
 def drive():
@@ -56,12 +62,100 @@ def gimble_ctrl():
     return "hub.port.A.motor.pwm({});hub.port.B.motor.pwm({});".format(vertical, horizontal)
 
 
+def define_gimble_read(board):
+    board.exec_raw(
+        "def gimble_read(motorA, motorB, dist_sensor):"
+        "return '{};{};{}'.format(motorA.motor.get()[0],motorB.motor.get()[0],dist_sensor.device.get()[0])"
+    )
+
+
+def gimble_read(board):
+    d, e = board.exec_raw("print(gimble_read(motorA,motorB,dist_sensor),end='')")
+    e = e.decode("utf8")
+    if e != "":
+        print(e)
+        return None
+
+    a, b, c = d.decode("utf8").split(";")
+    a = float(a) / 180.0 * math.pi / 3.0
+    b = float(b) / 180.0 * math.pi / 3.0
+    c = 200 if c == "None" else int(c)
+
+    l = c * math.cos(a)
+    x = l * math.cos(b)
+    y = l * math.sin(b)
+    z = c * math.sin(a)
+
+    return x, y, z
+
+
+def define_port(board, port, type, name):
+    d = None
+    e = 'Not none'
+    while d is None or e != "":
+        d, e = board.exec_raw("hub.port.{}.{}.get()".format(port, type))
+        d = d.decode('ascii')
+        e = e.decode('ascii')
+        if e != '':
+            print(e)
+        time.sleep(0.5)
+
+    board.exec_raw("{} = hub.port.{}".format(name, port))
+    print("Defined {}=hub.port.{}".format(name, port))
+
+
+class Plot:
+    def __init__(self):
+        self._z = np.array([])
+        self._x = np.array([])
+        self._y = np.array([])
+        self._prev = None
+
+    def add(self, x, y, z):
+        if self._prev == (x, y, z):
+            return
+
+        self._prev = (x, y, z)
+
+        self._x = np.append(self._x, x)
+        self._y = np.append(self._y, y)
+        self._z = np.append(self._z, z)
+
+    def show(self):
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        ax.set_xlim3d(-200, 200)
+        ax.set_ylim3d(-200, 200)
+        ax.set_zlim3d(0, 210)
+
+        ax.scatter3D(self._x, self._y, self._z, cmap='Greens')
+
+        plt.show()
+
+    def print(self):
+        print(self._x)
+        print(self._y)
+        print(self._z)
+
+
 if __name__ == "__main__":
     print("Connecting")
     board = pyboard.Pyboard("COM3")
     board.enter_raw_repl()
 
     board.exec_raw("import hub")
+    board.exec_raw("import time")
+
+    define_port(board, "A", "motor", "motorA")
+    define_port(board, "B", "motor", "motorB")
+    define_port(board, "C", "device", "dist_sensor")
+
+    print(board.exec_raw("motorA.motor.mode(3);motorB.motor.mode(3)"))
+
+    define_gimble_read(board)
+
+    plot = Plot()
+    # board.exec_raw("from mindstorms import DistanceSensor")
 
     print("Connected")
 
@@ -75,6 +169,8 @@ if __name__ == "__main__":
             KeyA: False,
             KeyS: False,
             KeyD: False,
+            KeyP: False,
+            KeyO: False
     }
 
     def on_press(key):
@@ -85,20 +181,29 @@ if __name__ == "__main__":
         if key in keys:
             keys[key] = False
 
-    prev_command = time.monotonic_ns()
+    prev_command = time.monotonic()
+    prev_draw = time.monotonic()
     with Listener(on_press=on_press, on_release=on_release) as listener:
         print("Started listening")
         while not keys[Key.esc]:
             drive_command = drive()
             gimble_command = gimble_ctrl()
 
-            delta = time.monotonic_ns() - prev_command
-            if delta > 500:
+            delta = time.monotonic() - prev_command
+            if delta > 0.05:
                 board.exec_raw(str.encode(drive_command + ";" + gimble_command))
 
+                gimble_pos = gimble_read(board)
+                if gimble_pos is not None:
+
+                    plot.add(*gimble_read(board))
+
                 prev_command = time.monotonic()
-            else:
-                print(delta)
+
+            draw_delta = time.monotonic() - prev_draw
+            if draw_delta > 0.5:
+                plot.show()
+                prev_draw = time.monotonic()
 
         listener.stop()
         print("Stopped listening")
